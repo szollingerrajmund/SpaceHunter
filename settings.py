6b517f6c656913e2c2ff1502c5776c84d46a7367
majorbalintistvan <major.balint.istvan@students.jedlik.eu>
2024-03-13 12:52:47 +0100

    max gyorsaság konfigurálása

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from pygame import Vector2
 WIDTH: int = 1600
 HEIGHT: int = 800
 FPS: int = 60
-MANEUVERABILITY: float = 3.4
+MANEUVERABILITY: float = 3
 UP: Vector2 = Vector2(0, -1)
 SPEED: float = 0.15
-MAX_SPEED: int = 8
+MAX_SPEED:float = 7.5
