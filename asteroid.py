444f845437c10cb413f938730112aab4cbe0380d
baracskai.dora <baracskai.dora@students.jedlik.eu>
2024-03-14 12:46:01 +0100

    aszteroida konflikt megoldás

--- asteroid.py
+++ asteroid.py
@@ -1,3 +1,4 @@
+from ctypes.wintypes import RGB
 import pygame
 
 
@@ -21,15 +22,26 @@ class Asteroid(object):
         self.image: pygame.Surface = self.images[self.frame]
 
     def draw(self, screen: pygame.Surface) -> None:
-        blit_position = self.image.get_rect(center=self.position)
+
+        blit_position: pygame.Rect = self.image.get_rect(center=self.position)
         screen.blit(self.image, blit_position)
+        pygame.draw.rect(screen, RGB(0, 255, 255), blit_position, 3)
 
     def update(self, screen: pygame.Surface) -> None:
         self.animation()
         self.draw(screen)
+        self.moving(screen)
 
     def animation(self) -> None:
         self.frame += self.changing
         if self.frame >= len(self.images):
             self.frame = 0
-        self.image = self.images[int(self.frame)]
+        self.image = pygame.transform.scale(self.images[int(self.frame)], (300, 300))
+
+    def moving(self, screen: pygame.Surface):
+        self.position = self.wrap_position(self.position + self.velocity, screen)
+
+    def wrap_position(self, position: pygame.Vector2, screen: pygame.Surface):
+        x, y = position
+        w, h = screen.get_size()
+        return pygame.Vector2(x % w + 0.5, y % h + 0.5)
