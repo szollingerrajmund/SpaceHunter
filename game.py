46cf8b5f9b59cede849c6424737ac46d9bc4abaa
majorbalintistvan <major.balint.istvan@students.jedlik.eu>
2024-04-10 12:47:33 +0200

    bug fix

--- game.py
+++ game.py
@@ -15,10 +15,10 @@ class Game(object):
         self.screen_res = (WIDTH, HEIGHT)
         self.screen = pygame.display.set_mode(self.screen_res)
         self.clock = pygame.time.Clock()
-        self.player = Player(self.screen_res[0] // 2, self.screen_res[1] // 2, pygame.Vector2(0))
+        self.player = Player( self.screen_res[0] // 2, self.screen_res[1] // 2, pygame.Vector2(0))
         self.asteroid_spawn = pygame.USEREVENT + 20
-        pygame.time.set_timer(self.asteroid_spawn, 2500)
-        self.asteroid_list:list[Asteroid] = []
+        pygame.time.set_timer(self.asteroid_spawn, 1500)
+        self.asteroid_list: list[Asteroid] = []
         self.bullet_list: list[Bullets] = []
         self.clock: pygame.time.Clock = pygame.time.Clock()
         self.game_state = "start_menu"
@@ -30,7 +30,6 @@ class Game(object):
         self.blast_sound = self.soundeffect.load_sound("blast")
         self.music = Sound().music()
         self.run()
-        
 
     def run(self):
         animation_started = False
@@ -64,19 +63,23 @@ class Game(object):
 
             elif self.game_state == "game":
                 self.game_over = False
-                self.music=True
+                self.music = True
                 self.draw()
                 self.player.update(self.screen)
                 for blast in self.bullet_list:
                     blast.update(self.screen)
                 for asteroid in self.asteroid_list:
                     asteroid.update(self.screen)
-                    player_rect:pygame.Rect = self.player.image.get_rect(center=self.player.position)
+                    player_rect: pygame.Rect = self.player.image.get_rect(
+                        center=self.player.position
+                    )
                     if player_rect.colliderect(asteroid.hitbox):
                         self.game_state = "game_over"
                         self.player.velocity = pygame.Vector2(0, 0)
                     for blast in self.bullet_list:
-                        blast_rect:pygame.Rect=blast.image.get_rect(center=blast.position)
+                        blast_rect: pygame.Rect = blast.image.get_rect(
+                            center=blast.position
+                        )
                         if blast_rect.colliderect(asteroid.hitbox):
                             self.asteroid_list.remove(asteroid)
                             self.bullet_list.remove(blast)
@@ -89,13 +92,16 @@ class Game(object):
                 pygame.mixer.music.stop()
                 keys = pygame.key.get_pressed()
                 if keys[pygame.K_r]:
-                    self.player.position = pygame.Vector2(self.screen_res[0] // 2, self.screen_res[1] // 2)
+                    self.player.position = pygame.Vector2(
+                        self.screen_res[0] // 2, self.screen_res[1] // 2
+                    )
                     self.asteroid_list = []
                     self.player.velocity = pygame.Vector2(0, 0)
                     self.player.reset_rotation()
                     self.game_state = "start_menu"
                     pygame.mixer.music.play()
                     self.time.reset_time()
+                    self.time.score = 0
                 if keys[pygame.K_k]:
                     pygame.quit()
                     quit()
@@ -106,28 +112,29 @@ class Game(object):
     def _input_kezeles(self):
         keys = pygame.key.get_pressed()
         for event in pygame.event.get():
-            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            if event.type == pygame.QUIT or (
+                event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
+            ):
                 pygame.quit()
                 quit()
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 self.shoot()
             elif event.type == self.asteroid_spawn:
                 self.spawn_asteroids()
-        
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d] and self.game_state=="game":
+
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d] and self.game_state == "game":
             self.player.rotate(clockwise=True)
-        elif keys[pygame.K_LEFT] or keys[pygame.K_a] and self.game_state=="game":
+        elif keys[pygame.K_LEFT] or keys[pygame.K_a] and self.game_state == "game":
             self.player.rotate(clockwise=False)
-        if keys[pygame.K_UP] or keys[pygame.K_w] and self.game_state=="game":
+        if keys[pygame.K_UP] or keys[pygame.K_w] and self.game_state == "game":
             self.player.speed_up()
 
-
     def spawn_asteroids(self):
-        if self.game_state=="game":
+        if self.game_state == "game":
             rand_x = random.randint(0, WIDTH + 200)
             rand_y = random.randint(0, 1800)
             velocity = random.randint(1, 2)
-            asteroid:Asteroid = Asteroid(rand_x, rand_y, velocity)
+            asteroid: Asteroid = Asteroid(rand_x, rand_y, velocity)
             self.asteroid_list.append(asteroid)
             pygame.display.update()
         else:
@@ -139,14 +146,19 @@ class Game(object):
         self.screen.blit(background, (0, 0))
 
     def shoot(self):
-        if self.game_state=="game":
+        if self.game_state == "game":
             blast: Bullets = Bullets(self.player.position, self.player.direction)
             if len(self.bullet_list) < 3:
                 self.bullet_list.append(blast)
                 self.blast_sound.play()
                 self.blast_sound.set_volume(0.2)
             for blast in self.bullet_list:
-                if ( blast.position.x < 1600 and blast.position.x > 0 and blast.position.y < 900 and blast.position.y > 0):
+                if (
+                    blast.position.x < 1600
+                    and blast.position.x > 0
+                    and blast.position.y < 900
+                    and blast.position.y > 0
+                ):
                     blast.move()
                 else:
                     self.bullet_list.remove(blast)
